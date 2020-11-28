ca6aa5654fc673a59f652541a7b652965b3f8113
leo <leo.kalshteyn@gmail.com>
2020-11-27 17:24:40 -0800

    added form for forecast button with weather data

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -30,6 +30,37 @@ const Forecast = () => {
    return (
      <div>
          <h2>Find Current Weather Conditions</h2>
+         <form onSubmit={getForecast}>
+             <input
+                 type="text"
+                 placeholder="Enter City"
+                 maxLength="50"
+                 value={city}
+                 onChange={(e) => setCity(e.target.value)}
+                 />
+             <label>
+                 <input
+                     type="radio"
+                     name="units"
+                     checked={unit === "imperial"}
+                     value="imperial"
+                     onChange={(e) => setUnit(e.target.value)}
+                     />
+                 Fahrenheit
+             </label>
+             <label>
+                 <input
+                     type="radio"
+                     name="units"
+                     checked={unit === "metric"}
+                     value="metric"
+                     onChange={(e) => setUnit(e.target.value)}
+                     />
+                 Celcius
+             </label>
+             <button type="submit">Get Forecast</button>
+         </form>
+
          <button onClick={getForecast}>Get Forecast</button>
          <Conditions
              responseObj={responseObj}
