13a2d3b84fe5f5bdf27477fc2df2bf1789f349e0
leo <leo.kalshteyn@gmail.com>
2020-11-27 17:03:49 -0800

    edit url string in forecast's fetch call

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -3,10 +3,15 @@ import Conditions from '../Conditions/Conditions';
 
 const Forecast = () => {
 
+   let [city, setCity] = useState('');
+   let [unit, setUnit] = useState('imperial');
    let [responseObj, setResponseObj] = useState({});
 
    function getForecast() {
-     fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Seattle", {
+
+     const uriEncodedCity = encodeURIComponent(city);
+
+     fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
      	"method": "GET",
      	"headers": {
      		"x-rapidapi-key": "8e8228baa9msh389459f1ffc6789p1afc37jsn450d3afe7532",
@@ -21,6 +26,7 @@ const Forecast = () => {
      	console.error(err);
      });
    }
+
    return (
      <div>
          <h2>Find Current Weather Conditions</h2>
@@ -31,4 +37,5 @@ const Forecast = () => {
      </div>
    )
 }
+
 export default Forecast;
