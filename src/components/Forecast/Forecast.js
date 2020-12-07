2f1ca53269f91cdd7826d5d6e998b08f4e8f1dce
leo <leo.kalshteyn@gmail.com>
2020-12-06 16:58:40 -0800

    added logo component

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -23,7 +23,7 @@ const Forecast = () => {
        fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
        	"method": "GET",
        	"headers": {
-       		"x-rapidapi-key": "8e8228baa9msh389459f1ffc6789p1afc37jsn450d3afe7532",
+       		"x-rapidapi-key": process.env.REACT_APP_API_KEY,
        		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
        	}
        })
